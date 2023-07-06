23816f016996b09a6ec40294d31c0edcbf53e4b2
harshitabhuiit <mandava.harshita.mec21@itbu.ac.in>
2023-07-06 13:39:29 +0530

    commit

---
+++ validate login.js
@@ -0,0 +1,12 @@
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    if (username === "" || password === "") {
+      alert("Please enter both username and password.");
+      return false;
+    }
+  
+    alert("Login successful!");
+    return true;
+  }
